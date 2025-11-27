091785640a55af0a9618cf813e61588013e0c920
Alvaro Alves <vindevsa@hotmail.com>
2025-11-27 12:30:54 -0300

    change: Add Node.js server code to file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const http = require("http");
+const fs = require("fs");
 
+http.createServer((req, res) => {
+    fs.readFile(__dirname + "/app.html");
+}).listen(3000, error => console.log(error || "Server running in http://localhost:3000"));
