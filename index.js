67685491a0848a56d3d3a8495fb4c937a1028a5c
Alvaro Alves <vindevsa@hotmail.com>
2025-11-27 12:42:25 -0300

    fix: Serve the page inside respose

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const http = require("http");
 const fs = require("fs");
 
 http.createServer((req, res) => {
-    fs.readFile(__dirname + "/app.html");
+    res.end(fs.readFileSync(__dirname + "/app.html"));
 }).listen(3000, error => console.log(error || "Server running in http://localhost:3000"));
