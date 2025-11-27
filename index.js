340fa7e15ff825371cae73aa7b198b8ed11a4219
Alvaro Alves <vindevsa@hotmail.com>
2025-11-27 12:50:39 -0300

    change: Add /style and /script routes using fs.readFileSync() method

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const http = require("http");
 const fs = require("fs");
 
 http.createServer((req, res) => {
-    res.end(fs.readFileSync(__dirname + "/app.html"));
+    req.url === "/" ? res.end(fs.readFileSync(__dirname + "/app.html")) :
+        req.url === "/style" ? res.end(fs.readFileSync(__dirname + "/css/index.css")) :
+            req.url === "/script" ? res.end(fs.readFileSync(__dirname + "/js/index.js")) :
+                res.end("404 Not Found");
 }).listen(3000, error => console.log(error || "Server running in http://localhost:3000"));
