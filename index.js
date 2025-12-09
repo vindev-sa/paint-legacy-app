7b38a4bf533447e54f1a886ae4045126bb4629b9
Alvaro Alves <vindevsa@hotmail.com>
2025-12-09 13:46:22 -0300

    change: realocate paths and file strutures to /public directories

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require("http");
 const fs = require("fs");
 
 http.createServer((req, res) => {
-    req.url === "/" ? res.end(fs.readFileSync(__dirname + "/app.html")) :
-        req.url === "/style" ? res.end(fs.readFileSync(__dirname + "/css/index.css")) :
-            req.url === "/script" ? res.end(fs.readFileSync(__dirname + "/js/index.js")) :
+    req.url === "/" ? res.end(fs.readFileSync(__dirname + "/public/app.html")) :
+        req.url === "/style" ? res.end(fs.readFileSync(__dirname + "/public/assets/css/index.css")) :
+            req.url === "/script" ? res.end(fs.readFileSync(__dirname + "/public/assets/js/index.js")) :
                 res.end("404 Not Found");
 }).listen(3000, error => console.log(error || "Server running in http://localhost:3000"));
